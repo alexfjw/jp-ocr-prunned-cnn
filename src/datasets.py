Fixed bug with normalization

src/datasets.py
@@ -52,11 +52,11 @@ class Etl2Dataset(Dataset):
         self.train_transforms = train_transforms
         self.test_transforms = test_transforms
 
-        self.entries = self.load_entries_to_memory()
+        self._entries = self.load_entries_to_memory()
         self.classes, self.class_to_idx = self.load_class_data()
 
     def load_class_data(self):
-        classes = list({label for _, label in self.entries})
+        classes = list({label for _, label in self._entries})
         class_to_idx = {classes[i]: i for i in range(len(classes))}
         return classes, class_to_idx
 
@@ -97,7 +97,6 @@ class Etl2Dataset(Dataset):
 
                     # image is grayscale, use otsu's algorithm to binarize it
                     pil_image = Image.frombytes('F', image_size, item_data[16], 'bit', bits_per_pixel)
-                    pil_image = pil_image.convert('RGB')
                     # np_image = np.array(pil_image)
                     # global_threshold = filters.threshold_otsu(np_image)
                     # binarized_image = np_image > global_threshold
@@ -116,10 +115,10 @@ class Etl2Dataset(Dataset):
             return entries
 
     def calculate_mean(self):
-        return np.mean([ImageStat.Stat(image).mean[0] for image, _ in self.entries])
+        return np.mean([ImageStat.Stat(image).mean[0] for image, _ in self._entries])
 
     def calculate_std(self):
-        return np.mean([ImageStat.Stat(image).stddev[0] for image, _ in self.entries])
+        return np.mean([ImageStat.Stat(image).stddev[0] for image, _ in self._entries])
 
     def __len__(self):
         def sum_file_count(sum_so_far, file_with_count):
@@ -128,8 +127,8 @@ class Etl2Dataset(Dataset):
         return reduce(sum_file_count, self.files, 0)
 
     def __getitem__(self, idx):
-        label = self.entries[idx].label
-        image = self.entries[idx].pil_image
+        label = self._entries[idx].label
+        image = self._entries[idx].pil_image
 
         if self.train and self.train_transforms:
             image = self.train_transforms(image)
@@ -146,7 +145,6 @@ class Etl9bDataset(Dataset):
     """
     mean = 63.243677227077974
     std = 16.8771143038
-    num_classes = 3036
 
     def __init__(self, train_transforms=None, test_transforms=None):
         # files to item count
@@ -156,7 +154,7 @@ class Etl9bDataset(Dataset):
                       ('data/ETL9B/ETL9B_4', 121440),
                       ('data/ETL9B/ETL9B_5', 121440+3036)
                       ]
-        self.entries = []
+        self._entries = []
         self.train = True
         self.train_transforms = train_transforms
         self.test_transforms = test_transforms
@@ -182,7 +180,7 @@ class Etl9bDataset(Dataset):
                     label = (b'\x1b$B' + bytes.fromhex(hex(r[1])[2:])).decode('iso2022_jp')
                     pil_image = Image.frombytes('1', image_size, r[3], 'raw')
 
-                    self.entries.append(
+                    self._entries.append(
                         CharacterEntry(pil_image=pil_image,
                                        label=label)
                     )
@@ -201,8 +199,8 @@ class Etl9bDataset(Dataset):
         return reduce(sum_file_count, self.files, 0)
 
     def __getitem__(self, idx):
-        label = self.entries[idx].label
-        image = self.entries[idx].pil_image
+        label = self._entries[idx].label
+        image = self._entries[idx].pil_image
 
         if self.train and self.train_transforms:
             image = self.train_transforms(image)

src/main.py
@@ -4,6 +4,7 @@ from torch.optim import lr_scheduler
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler, BatchSampler
 from torchvision import transforms
+import utils.transforms
 from utils.model_selection import stratified_test_split
 from sklearn.metrics import f1_score
 from models.models import vgg_model, chinese_model
@@ -18,11 +19,13 @@ import torch.backends.cudnn as cudnn
 
 cudnn.benchmark = True
 
+# ToFloat is required for normalization, tensor may be of type int depending on pil mode
 transfer_learn_etl2_transforms = {
     'train': transforms.Compose([
         transforms.Resize(224),
         transforms.Grayscale(num_output_channels=3),  # duplicate the channels
         transforms.ToTensor(),
+        utils.transforms.ToFloat,
         transforms.Normalize((Etl2Dataset.mean, Etl2Dataset.mean, Etl2Dataset.mean),
                              (Etl2Dataset.std, Etl2Dataset.std, Etl2Dataset.std))
     ]),
@@ -30,6 +33,7 @@ transfer_learn_etl2_transforms = {
         transforms.Resize(224),
         transforms.Grayscale(num_output_channels=3),  # duplicate the channels
         transforms.ToTensor(),
+        utils.transforms.ToFloat,
         transforms.Normalize((Etl2Dataset.mean, Etl2Dataset.mean, Etl2Dataset.mean),
                              (Etl2Dataset.std, Etl2Dataset.std, Etl2Dataset.std))
     ])
@@ -40,6 +44,7 @@ transfer_learn_etl9b_transforms = {
         transforms.Resize(224),
         transforms.Grayscale(num_output_channels=3),  # duplicate the channels
         transforms.ToTensor(),
+        utils.transforms.ToFloat,
         transforms.Normalize((Etl9bDataset.mean, Etl9bDataset.mean, Etl9bDataset.mean),
                              (Etl9bDataset.std, Etl9bDataset.std, Etl9bDataset.std))
     ]),
@@ -47,33 +52,36 @@ transfer_learn_etl9b_transforms = {
         transforms.Resize(224),
         transforms.Grayscale(num_output_channels=3),  # duplicate the channels
         transforms.ToTensor(),
+        utils.transforms.ToFloat,
         transforms.Normalize((Etl9bDataset.mean, Etl9bDataset.mean, Etl9bDataset.mean),
                              (Etl9bDataset.std, Etl9bDataset.std, Etl9bDataset.std))
     ])
 }
 
-chinese_transforms = {
+chinese_transforms_etl2 = {
     'train': transforms.Compose([
         transforms.Resize(96),
         transforms.ToTensor(),
-        transforms.Normalize((Etl2Dataset.mean, Etl2Dataset.mean, Etl2Dataset.mean),
-                             (Etl2Dataset.std, Etl2Dataset.std, Etl2Dataset.std))
+        utils.transforms.ToFloat,
+        transforms.Normalize([Etl2Dataset.mean], [Etl2Dataset.std])
     ]),
     'test': transforms.Compose([
         transforms.Resize(96),
         transforms.ToTensor(),
-        transforms.Normalize((Etl2Dataset.mean, Etl2Dataset.mean, Etl2Dataset.mean),
-                             (Etl2Dataset.std, Etl2Dataset.std, Etl2Dataset.std))
+        utils.transforms.ToFloat,
+        transforms.Normalize([Etl2Dataset.mean], [Etl2Dataset.std])
     ])
 }
 
 
-def get_etl2_dataloaders():
+def get_etl2_dataloaders(model_type):
     """
     returns train & test dataloaders for etl2 dataset
     """
-    etl2 = Etl2Dataset(train_transforms=transfer_learn_etl2_transforms['train'],
-                       test_transforms=transfer_learn_etl2_transforms['test'])
+    transform_group = transfer_learn_etl2_transforms if model_type == 'vgg' else chinese_transforms_etl2
+
+    etl2 = Etl2Dataset(train_transforms=transform_group['train'],
+                       test_transforms=transform_group['test'])
     train_indices, val_indices, test_indices, _, _, _ = \
         stratified_test_split(etl2, test_size=0.2, val_size=0.2)
 
@@ -217,7 +225,7 @@ def get_args():
 def main():
     args = get_args()
 
-    data_loaders, num_classes = get_etl2_dataloaders()
+    data_loaders, num_classes = get_etl2_dataloaders(args.model)
 
     if args.train:
         model, name = vgg_model(num_classes) if args.model == "vgg" \